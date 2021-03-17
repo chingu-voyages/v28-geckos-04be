cdb604e6bd4069df63be6d827be839e47cfb87a8
monika <thanki.monika@gmail.com>
2021-03-17 09:03:00 -0700

    jwt auth

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://chingu@localhost/geckos',
+    API_TOKEN: process.env.API_TOKEN,
+    JWT_SECRET: process.env.JWT_SECRET|| 'v28-mushroom-finder-jwt',
+    JWT_EXPIRY: process.env.JWT_EXPIRY 
+
 }
